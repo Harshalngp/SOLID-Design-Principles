e58066c2f8a9e806d69b82fa5eaa814d511a4fd7
Webhunter <harshalhiwarkar01@gmail.com>
2022-01-06 16:43:36 +0530

    SOLID Principle

---
+++ Interface_Substitution_Principle.py
@@ -0,0 +1,40 @@
+'''
+Interface Segregation Principle
+
+Interfaces should be granularly split and be as small as possible.
+'''
+
+from abc import ABCMeta, abstractmethod
+
+class CallingDevice():
+  @abstractmethod
+  def make_calls():
+    pass
+
+class MessagingDevice():
+  @abstractmethod
+  def send_sms():
+    pass
+
+class InternetbrowsingDevice():
+  @abstractmethod
+  def browse_internet():
+    pass
+
+class SmartPhone(CallingDevice, MessagingDevice, InternetbrowsingDevice):
+  def make_calls():
+    #implementation
+    pass
+
+  def send_sms():
+    #implementation
+    pass
+
+  def browse_internet():
+    #implementation
+    pass
+
+class LandlinePhone(CallingDevice):
+  def make_calls():
+    #implementation
+    pass
---
+++ Single_Responsibility_Principle.py
@@ -0,0 +1,46 @@
+'''
+Single Responsibility Principle
+
+A class should have ONE responsibility and no more.
+'''
+
+class TelephoneDirectory:
+  def __init__(self):
+    self.telephonedirectory = {}
+
+  def add_entry(self, name, number):
+    self.telephonedirectory[name] = number
+
+  def delete_entry(self, name):
+    self.telephonedirectory.pop(name)
+
+  def update_entry(self, name, number):
+    self.telephonedirectory[name] = number
+
+  def lookup_number(self, name):
+    return self.telephonedirectory[name]
+
+  def __str__(self):
+    ret_dct = ""
+    for key, value in self.telephonedirectory.items():
+      ret_dct += f'{key} : {value}\n'
+    return ret_dct
+
+class persist_to_database:
+  def __init__(self, object_to_persist):
+    pass
+
+class save_to_file:
+  def __init__(self, object_to_save):
+    pass
+
+myTelephoneDirectory = TelephoneDirectory()
+myTelephoneDirectory.add_entry("Ravi", 123456)
+myTelephoneDirectory.add_entry("Vikas", 678452)
+print(myTelephoneDirectory)
+
+myTelephoneDirectory.delete_entry("Ravi")
+myTelephoneDirectory.add_entry("Ravi", 123456)
+myTelephoneDirectory.update_entry("Vikas", 776589)
+print(myTelephoneDirectory.lookup_number("Vikas"))
+print(myTelephoneDirectory)
